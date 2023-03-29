e8793ee57f4f12b6af997d26ac3e6628d017effd
ArpanBhowmik <bhowmik.arpan268@gmail.com>
2023-03-29 22:30:38 +0600

    Added Animation and Sound Effects

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -13,6 +13,9 @@ struct OnboardingView: View {
     @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
+    @State private var imageOffset: CGSize = .zero
+    @State private var indicatorOpacity: Double = 1.0
+    @State private var title: String = "Share."
     
     var body: some View {
         ZStack {
@@ -23,10 +26,12 @@ struct OnboardingView: View {
                 // MARK: - HEADER
                 
                 VStack(spacing: 0) {
-                    Text("Share.")
+                    Text(title)
                         .font(.system(size: 60))
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
+                        .transition(.opacity)
+                        .id(title) // This line tells swift UI to consider a new view when changing the title text
                     
                     Text("""
                     It's not how much we give but
@@ -46,12 +51,46 @@ struct OnboardingView: View {
                 
                 ZStack {
                     CircleGroupView(shapeColor: .white, shapeOpacity: 0.2)
+                        .offset(x: imageOffset.width * -1)
+                        .blur(radius: imageOffset.width / 5.0)
+                        .animation(.easeOut(duration: 1), value: imageOffset)
                     
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeOut(duration: 0.5), value: isAnimating)
+                        .offset(x: imageOffset.width * 1.2, y: 0)
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if abs(imageOffset.width) <= 150 {
+                                        imageOffset = gesture.translation
+                                        withAnimation(.linear(duration: 0.25)) {
+                                            indicatorOpacity = 0
+                                            title = "Give."
+                                        }
+                                    }
+                                }
+                                .onEnded { _ in
+                                    imageOffset = .zero
+                                    withAnimation(.linear(duration: 0.25)) {
+                                        indicatorOpacity = 1
+                                        title = "Share."
+                                    }
+                                }
+                        )
+                        .animation(.easeOut(duration: 1), value: imageOffset)
+                }
+                .overlay(alignment: .bottom) {
+                    Image(systemName: "arrow.left.and.right.circle")
+                        .font(.system(size: 44, weight: .ultraLight))
+                        .foregroundColor(.white)
+                        .offset(y: 20)
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+                        .opacity(indicatorOpacity)
                 }
                 
                 // MARK: - Footer
@@ -100,6 +139,7 @@ struct OnboardingView: View {
                                         if buttonOffset <= buttonWidth / 2.0 {
                                             buttonOffset = 0
                                         } else {
+                                            playSound(sound: "chimeup", type: "mp3")
                                             buttonOffset = buttonWidth - 80
                                             isOnboardingActive = false
                                         }
@@ -115,7 +155,6 @@ struct OnboardingView: View {
                         .fontWeight(.bold)
                         .foregroundColor(.white)
                         .offset(x: 20)
-                    
                 }
                 .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
@@ -127,6 +166,7 @@ struct OnboardingView: View {
         .onAppear {
             isAnimating = true
         }
+        .preferredColorScheme(.dark)
     }
 }
 
